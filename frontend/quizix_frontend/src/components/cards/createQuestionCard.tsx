b9c460eba4a819a2afbae5a5a5af668c1e8f7c12
saurabhacharya24 <saurabhacharya123@gmail.com>
2019-09-29 16:25:59 +1000

    Added CreateQuestionCard and DatePicker in CreateQuiz
    Choices can now be added and removed as well

---
+++ frontend/quizix_frontend/src/components/cards/createQuestionCard.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+interface State {
+    questionText: string
+    choices: Array<JSX.Element>
+    numChoices: number
+}
+
+interface Props {
+    qNum: number
+    removeQuestion(evt: any): void
+}
+
+class CreateQuestionCard extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props)
+
+        this.state = {
+            questionText: "",
+            choices: [],
+            numChoices: 0
+        }
+
+        this.increaseChoices = this.increaseChoices.bind(this)
+        this.decreaseChoices = this.decreaseChoices.bind(this)
+    }
+
+    componentDidMount() {
+        this.increaseChoices()
+        this.increaseChoices()
+    }
+
+    changeQuestionTextState = (evt: any) => {
+        let question: string = evt.target.value
+        this.setState({ questionText: question })
+    }
+    
+    increaseChoices() {
+        let { choices, numChoices } = this.state
+        choices.push(this.renderChoice(numChoices+1))
+
+        this.setState({
+            choices: choices,
+            numChoices: numChoices+1
+        })
+    }
+
+    decreaseChoices(evt: any) {
+        let { choices, numChoices } = this.state
+        let choiceNum = evt.target.id
+        let count = 0;
+
+        let arr1: Array<JSX.Element> = []
+        let arr2: Array<JSX.Element> = []
+        
+        choices.forEach(c => {
+            if (choiceNum === c.key) {
+                arr1 = choices.slice(0,count)
+                arr2 = choices.slice(count+1, choices.length)
+            }
+            else {
+                count++
+            }
+        })
+        
+        let finalArr = arr1.concat(arr2)
+        this.setState({
+            choices: finalArr,
+            numChoices: numChoices-1
+        })
+    }
+
+    renderChoice(choiceNum: number): JSX.Element {
+        return (
+            <div className="choice" key={choiceNum}>
+                <span className="remove-choice" onClick={this.decreaseChoices} id={choiceNum.toString()}> &times; </span>
+                <input className="choice-input" placeholder="Enter choice..." />
+            </div>
+        )
+    }
+
+    render() {
+        let { qNum, removeQuestion } = this.props
+        let { choices } = this.state
+
+        return (
+            <div className="question-box">
+                <span className="remove-question" onClick={removeQuestion} id={qNum.toString()}> &times; </span> 
+                <p className="question-text"> Question: </p>
+                <textarea className="question" placeholder="Enter question..." onChange={this.changeQuestionTextState}/>
+                <p className="choices-text"> Choices (minimum 2): </p>
+                <div className="choices-box">
+                    {choices.map(choice => {
+                        return choice
+                    })}
+                </div>
+                <button className="increase-choices" onClick={this.increaseChoices}> Add Choice </button>
+            </div>
+        )
+    }
+}
+
+export default CreateQuestionCard
--- frontend/quizix_frontend/src/components/quizPages/createQuiz.tsx
+++ frontend/quizix_frontend/src/components/quizPages/createQuiz.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import CreateQuestionCard from '../cards/createQuestionCard';
 // import axios from 'axios'
 
 interface State {
     quizName: string
     quizDesc: string
-    startDateTime: Date
-    endDateTime: Date
+    startDate: Date
+    endDate: Date
     reviewDate: Date
+    questions: Array<JSX.Element>
 }
 
 interface Props {
@@ -23,20 +25,57 @@ class CreateQuiz extends React.Component<Props, State> {
         this.state = {
             quizName: "",
             quizDesc: "",
-            startDateTime: new Date(),
-            endDateTime: new Date(),
-            reviewDate: new Date()
+            startDate: new Date(),
+            endDate: new Date(),
+            reviewDate: new Date(),
+            questions: []
         }
 
         this.changeQuizNameState = this.changeQuizNameState.bind(this)
         this.changeQuizDescState = this.changeQuizDescState.bind(this)
+        this.incQuestions = this.incQuestions.bind(this)
+        this.decQuestions = this.decQuestions.bind(this)
     }
 
     changeQuizNameState(evt: any) { this.setState({ quizName: evt.currentTarget.value }) }
     changeQuizDescState(evt: any) { this.setState({ quizDesc: evt.currentTarget.value }) }
-    changeStartDateTimeState = (date: Date) => { this.setState({ startDateTime: date }) }
-    changeEndDateTimeState = (date: Date) => { this.setState({ endDateTime: date }) }
+    changeStartDateState = (date: Date) => { this.setState({ startDate: date }) }
+    changeEndDateState = (date: Date) => { this.setState({ endDate: date }) }
     changeReviewDateState = (date: Date) => { this.setState({ reviewDate: date }) }
+
+    incQuestions() {
+        let { questions } = this.state
+        let qNumNow = questions.length+1
+        questions.push(
+            <CreateQuestionCard
+                key={qNumNow}
+                qNum={qNumNow}
+                removeQuestion={this.decQuestions}
+            />)
+        this.setState({ questions: questions })
+    }
+
+    decQuestions(evt: any) {
+        let { questions } = this.state
+        let qNum = evt.target.id
+        let count = 0;
+
+        let arr1: Array<JSX.Element> = []
+        let arr2: Array<JSX.Element> = []
+        
+        questions.forEach(q => {
+            if (qNum === q.key) {
+                arr1 = questions.slice(0,count)
+                arr2 = questions.slice(count+1, questions.length)
+            }
+            else {
+                count++
+            }
+        })
+        
+        let finalArr = arr1.concat(arr2)
+        this.setState({ questions: finalArr })
+    }
 
     renderQuizNameAndDesc() {
         return (
@@ -57,10 +96,10 @@ class CreateQuiz extends React.Component<Props, State> {
         return (
             <div>
                 <div className="start-date-box">
-                    <p className="start-date"> Start Date and Time: </p>
+                    <p className="start-date"> Starts on: </p>
                     <DatePicker
-                        selected={this.state.startDateTime}
-                        onChange={this.changeStartDateTimeState}
+                        selected={this.state.startDate}
+                        onChange={this.changeStartDateState}
                         className="start-date-input"
                         minDate={new Date()}
                         showTimeSelect
@@ -78,12 +117,12 @@ class CreateQuiz extends React.Component<Props, State> {
         return (
             <div>
                  <div className="end-date-box">
-                    <p className="end-date"> End Date and Time: </p>
+                    <p className="end-date"> Ends on: </p>
                     <DatePicker
-                        selected={this.state.endDateTime}
-                        onChange={this.changeEndDateTimeState}
+                        selected={this.state.endDate}
+                        onChange={this.changeEndDateState}
                         className="end-date-input"
-                        minDate={new Date()}
+                        minDate={this.state.startDate}
                         showTimeSelect
                         timeFormat="HH:mm"
                         timeIntervals={15}
@@ -95,12 +134,38 @@ class CreateQuiz extends React.Component<Props, State> {
         )
     }
 
+    renderReviewDate() {
+        return (
+            <div>
+                <div className="review-date-box">
+                    <p className="review-date"> Can be reviewed after: </p>
+                    <DatePicker
+                        selected={this.state.reviewDate}
+                        onChange={this.changeReviewDateState}
+                        className="review-date-input"
+                        minDate={this.state.endDate}
+                        dateFormat="dd MMMM yyyy"
+                    />
+                </div>
+            </div>
+        )
+    }
+
     render() {
+        let { questions } = this.state
+
         return (
             <div className="create-quiz-main">
                 {this.renderQuizNameAndDesc()}
                 {this.renderStartDatetime()}
                 {this.renderEndDatetime()}
+                {this.renderReviewDate()}
+                <p className="questions-main-title"> Questions </p>
+                <button className="add-question" onClick={this.incQuestions}> + Add Question </button>
+                {questions.map(question => {
+                    return question
+                })}
+                <button className="submit-create-quiz"> Create! </button>
             </div>
         )
     }
